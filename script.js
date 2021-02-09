09098713974fe07cc60c260e8a842dfee88892b2
Hussnain25 <hussnain444all@gmail.com>
2021-02-09 17:21:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function ($) {
+		function animateElements() {
+			$('.progressbar').each(function () {
+				var elementPos = $(this).offset().top;
+				var topOfWindow = $(window).scrollTop();
+				var percent = $(this).find('.circle').attr('data-percent');
+				var percentage = parseInt(percent, 10) / parseInt(100, 10);
+				var animate = $(this).data('animate');
+				if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+					$(this).data('animate', true);
+					$(this).find('.circle').circleProgress({
+						startAngle: -Math.PI / 2,
+						value: percent / 100,
+						size: 130,
+						thickness: 10,
+						emptyFill: "#6CB4EE",
+						fill: {
+							color: '#002D62'
+						}
+					})/*.on('circle-animation-progress', function (event, progress, stepValue) {
+						$(this).find('div').text((stepValue*100).toFixed(1) + "%");
+					}).stop();*/
+				}
+			});
+		}
+
+		// Show animated elements
+		animateElements();
+		$(window).scroll(animateElements);
+		
+		
+		
+
+	}); //end document ready function
